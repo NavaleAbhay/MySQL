4424a5abba38649d7cd5b2d113e3ec25808415c2
Abhaynavale-6080 <navale.abhay12@gmail.com>
2022-12-06 15:29:30 +0530

    simpledbconnection

--- SimpleMySqlDBConnectionApp/Program.cs
+++ SimpleMySqlDBConnectionApp/Program.cs
@@ -4,15 +4,23 @@ MySqlConnection conn=new MySqlConnection();
 try{
     conn.ConnectionString=connStr;
     conn.Open();
-    string query="select * from employees";
-    MySqlCommand command=new MySqlCommand(query,conn);
-    MySqlDataReader reader=command.ExecuteReader();
+   // string query="select * from employees";
+    string query1="select * from members";
+   // MySqlCommand command=new MySqlCommand(query,conn);
+    MySqlCommand command1=new MySqlCommand(query1,conn);
+   // MySqlDataReader reader=command.ExecuteReader();
+    MySqlDataReader reader1=command1.ExecuteReader();
 
-    while(reader.Read())
+    // while(reader.Read())
+    // {
+    //     Console.WriteLine(reader[0]+" -- "+reader[1]+" -- "+reader[2]+" -- "+reader[3]);
+    // }
+    while(reader1.Read())
     {
-        Console.WriteLine(reader[0]+" -- "+reader[1]+" -- "+reader[2]+" -- "+reader[3]);
+        Console.WriteLine(reader1[0]+"--"+reader1[1]);
     }
-    reader.Close();
+   // reader.Close();
+    reader1.Close();
 }
 catch(Exception e){
     Console.WriteLine(e.Message);
