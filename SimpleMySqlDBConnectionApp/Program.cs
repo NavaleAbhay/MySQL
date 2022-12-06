c827fae92da8654f11aa22c672ef9bea33d04460
Abhaynavale-6080 <navale.abhay12@gmail.com>
2022-12-06 15:05:53 +0530

    simple mysql connection

---
+++ SimpleMySqlDBConnectionApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+string connStr="server=localhost;uid=root;password=@sonya6080;database=classicmodels";
+MySqlConnection conn=new MySqlConnection();
+try{
+    conn.ConnectionString=connStr;
+    conn.Open();
+    string query="select * from employees";
+    MySqlCommand command=new MySqlCommand(query,conn);
+    MySqlDataReader reader=command.ExecuteReader();
+
+    while(reader.Read())
+    {
+        Console.WriteLine(reader[0]+" -- "+reader[1]+" -- "+reader[2]+" -- "+reader[3]);
+    }
+    reader.Close();
+}
+catch(Exception e){
+    Console.WriteLine(e.Message);
+}
+finally{
+    conn.Close();
+}
